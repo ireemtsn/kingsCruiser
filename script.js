71c5b2372ac63c9996fdcda8507f8c9917396890
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-03-07 21:32:34 +0300

    artı ve eksi butonlarının düzenlenmesi ve çalıştırılması

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 /* Bilet tarihi seçimi*/
-let tarih = Date(prompt("gidiş ve dönüş tarihinizi seçiniz:"));
+/*let tarih = Date(prompt("gidiş ve dönüş tarihinizi seçiniz:"));
 
 
 let gidis = document.getElementById('departure');
+console.log(gidis);
 
 let donus = document.getElementById('return');
 
@@ -16,6 +17,99 @@ else{
 }
 
 console.log(donus);
+*/
+
+
+$(document).ready(function() {
+    $('#select1, #select2').change(function() {
+      var value1 = $('#select1').val();
+      var value2 = $('#select2').val();
+      if (value1 === value2) {
+        alert('The selected values must be different!');
+      }
+    });
+  });
+
+  $(document).ready(function(){
+  let vipBiletSayisi=  $('#vip');
+  $("#vipBiletArti").click(function(e)
+  { e.preventDefault()
+
+
+   vipBiletSayisi.val(parseInt(vipBiletSayisi.val())+1);
+  });
+
+
+  })
+
+
+  $(document).ready(function(){
+    let vipBiletSayisi=  $('#vip');
+    $("#vipBiletEksi").click(function(e)
+    { e.preventDefault()
+  
+  
+     vipBiletSayisi.val(parseInt(vipBiletSayisi.val())-1);
+     
+    });
+  
+  
+    });
+
+
+    $(document).ready(function(){
+        let vipBiletSayisi=  $('#firstClass');
+        $("#firstClassBiletArti").click(function(e)
+        { e.preventDefault()
+      
+      
+         vipBiletSayisi.val(parseInt(vipBiletSayisi.val())+1);
+        });
+      
+      
+        });
+
+        $(document).ready(function(){
+            let vipBiletSayisi=  $('#firstClass');
+            $("#firstClassBiletEksi").click(function(e)
+            { e.preventDefault()
+          
+          
+             vipBiletSayisi.val(parseInt(vipBiletSayisi.val())-1);
+            });
+          
+          
+            })
+
+            $(document).ready(function(){
+                let vipBiletSayisi=  $('#Economy');
+                $("#EconomyBiletEksi").click(function(e)
+                { e.preventDefault()
+              
+              
+                 vipBiletSayisi.val(parseInt(vipBiletSayisi.val())-1);
+                });
+              
+              
+                })
+
+                $(document).ready(function(){
+                    let vipBiletSayisi=  $('#Economy');
+                    $("#EconomyBiletArti").click(function(e)
+                    { e.preventDefault()
+                  
+                  
+                     vipBiletSayisi.val(parseInt(vipBiletSayisi.val())+1);
+                    });
+                  
+                  
+                    })
+      
+  
+
+  
+
+
 
 
 
