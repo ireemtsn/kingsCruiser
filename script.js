7b86dcd4fded561e41667fed5fdba3cc063aa512
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-03-07 12:07:48 +0300

    billet görüntüleme sayfasının oluşturulması

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-let vıp=document.getElementById('vıp');
+
+/* Bilet tarihi seçimi*/
+let tarih = Date(prompt("gidiş ve dönüş tarihinizi seçiniz:"));
+
+
+let gidis = document.getElementById('departure');
+
+let donus = document.getElementById('return');
+
+if(gidis==donus) {
+    console.log("yanlış dönüş tarihi seçtiniz");
+}
+
+else{
+    console.log("bilet seçebilirsiniz");
+}
+
+console.log(donus);
+
+
+
+
+
+
+
+
+
+/*Biet ücreti hesaplama */
+
+/*let vıp=document.getElementById('vıp');
 
 let totalsonuc=parseInt(document.getElementById('vıp')*700);
 console.log(totalsonuc);
 
-/*let toplam=document.getElementById('subtotal');
+let toplam=document.getElementById('subtotal');
 let subtotal= parseInt(vıp*7000);
 
 console.log(subtotal);
