076590d86e0396dde415172fcb379ac58a04fc26
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-03-07 11:29:46 +0300

    günncel durum

---
+++ script.js
@@ -0,0 +1,11 @@
+let vıp=document.getElementById('vıp');
+
+let totalsonuc=parseInt(document.getElementById('vıp')*700);
+console.log(totalsonuc);
+
+/*let toplam=document.getElementById('subtotal');
+let subtotal= parseInt(vıp*7000);
+
+console.log(subtotal);
+*/
+
