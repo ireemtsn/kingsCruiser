406a8d933fe30716a833af0722cc5b8655a06126
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-03-12 17:37:01 +0300

    kodların düzenlenmesi

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-
-
 /*GİDİŞ VE DÖNÜŞ AYNI YERE OLAMAZ! */
-
-$(document).ready(function () {
-   $('#select1, #select2').change(function () {
-      var value1 = $('#select1').val();
-      var value2 = $('#select2').val();
-      if (value1 === value2) {
-         alert('The selected values must be different!');
-      }
-      localStorage.setItem("select1", value1);
-      localStorage.setItem("select2", value2);
+$(document).ready(function() {
+   $('#select1, #select2').change(function() {
+       var value1 = $('#select1').val();
+       var value2 = $('#select2').val();
+       if (value1 === value2) {
+           alert('The selected values must be different!');
+       }
+       localStorage.setItem("select1", value1);
+       localStorage.setItem("select2", value2);
    });
 });
 
 /*GİDİŞ VE DÖNÜŞ YERLERİNİN STORAGE İLE YAZILMASI*/
 
-$(document).ready(function () {
+$(document).ready(function() {
    // Get the saved values from localStorage
    var value1 = localStorage.getItem('select1');
    var value2 = localStorage.getItem('select2');
    if (value1 === value2) {
-     alert('Aynı yere bilet kesilemez!');
-     // Reset the select elements
-     $('#select1').val('');
-     $('#select2').val('');
-     // Clear the saved values from localStorage
-     localStorage.removeItem('select1');
-     localStorage.removeItem('select2');
-     // Update the displayed values
-     $('#cruisingFrom').text('');
-     $('#cruisingTo').text('');
+       alert('Aynı yere bilet kesilemez!');
+       // Reset the select elements
+       $('#select1').val('');
+       $('#select2').val('');
+       // Clear the saved values from localStorage
+       localStorage.removeItem('select1');
+       localStorage.removeItem('select2');
+       // Update the displayed values
+       $('#cruisingFrom').text('');
+       $('#cruisingTo').text('');
+   } else {
+       // Set the text of the elements to the saved values
+       $('#cruisingFrom').text(value1);
+       $('#cruisingTo').text(value2);
    }
-   else {
-     // Set the text of the elements to the saved values
-     $('#cruisingFrom').text(value1);
-     $('#cruisingTo').text(value2);
-   }
- });
- 
+});
+
 
 /*BİLET SAYISINI ARTTIRMA İŞLEMİ BUTON CLİCK KULLANILDI!*/
 
-$(document).ready(function () {
+$(document).ready(function() {
    let vipBiletSayisi = $('#vip');
-   $("#vipBiletArti").click(function (e) {
-      e.preventDefault()
+   $("#vipBiletArti").click(function(e) {
+       e.preventDefault()
 
 
-      vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) + 1);
+       vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) + 1);
    });
 
 
@@ -57,13 +53,13 @@ $(document).ready(function () {
 
 /*BİLET SAYISINI AZALTMA İŞLEMİ BUTON CLİCK KULLANILDI!*/
 
-$(document).ready(function () {
+$(document).ready(function() {
    let vipBiletSayisi = $('#vip');
-   $("#vipBiletEksi").click(function (e) {
-      e.preventDefault()
+   $("#vipBiletEksi").click(function(e) {
+       e.preventDefault()
 
 
-      vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) - 1);
+       vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) - 1);
 
    });
 
@@ -71,48 +67,48 @@ $(document).ready(function () {
 });
 
 
-$(document).ready(function () {
+$(document).ready(function() {
    let vipBiletSayisi = $('#firstClass');
-   $("#firstClassBiletArti").click(function (e) {
-      e.preventDefault()
+   $("#firstClassBiletArti").click(function(e) {
+       e.preventDefault()
 
-      vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) + 1);
+       vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) + 1);
    });
 
 
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
    let vipBiletSayisi = $('#firstClass');
-   $("#firstClassBiletEksi").click(function (e) {
-      e.preventDefault()
+   $("#firstClassBiletEksi").click(function(e) {
+       e.preventDefault()
 
 
-      vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) - 1);
+       vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) - 1);
    });
 
 
 })
 
-$(document).ready(function () {
+$(document).ready(function() {
    let vipBiletSayisi = $('#Economy');
-   $("#EconomyBiletEksi").click(function (e) {
-      e.preventDefault()
+   $("#EconomyBiletEksi").click(function(e) {
+       e.preventDefault()
 
 
-      vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) - 1);
+       vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) - 1);
    });
 
 
 })
 
-$(document).ready(function () {
+$(document).ready(function() {
    let vipBiletSayisi = $('#Economy');
-   $("#EconomyBiletArti").click(function (e) {
-      e.preventDefault()
+   $("#EconomyBiletArti").click(function(e) {
+       e.preventDefault()
 
 
-      vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) + 1);
+       vipBiletSayisi.val(parseInt(vipBiletSayisi.val()) + 1);
    });
 
 
@@ -120,17 +116,17 @@ $(document).ready(function () {
 
 /*BUGÜN VE GEÇMİŞ TARİHİ SEÇEMEZSİNİZ BÖLÜMÜ"*/
 
-$(document).ready(function () {
-   $('#datetime-local, #datetime-local2').change(function () {
-      var value1 = $('#datetime-local').val();
-      var value2 = $('#datetime-local2').val();
-      var value1 = new Date($(this).val());
-      var value2 = new Date($(this).val());
-      var now = new Date();
-      if (value1 === value2) {
-         alert('The selected values must be different!');
-      } else if (value1 <= now || value2 <= now)
-         alert('bugünü ve geçmiş tarihi seçemezsiniz');
+$(document).ready(function() {
+   $('#datetime-local, #datetime-local2').change(function() {
+       var value1 = $('#datetime-local').val();
+       var value2 = $('#datetime-local2').val();
+       var value1 = new Date($(this).val());
+       var value2 = new Date($(this).val());
+       var now = new Date();
+       if (value1 === value2) {
+           alert('The selected values must be different!');
+       } else if (value1 <= now || value2 <= now)
+           alert('bugünü ve geçmiş tarihi seçemezsiniz');
    });
 
 });
@@ -138,8 +134,7 @@ $(document).ready(function () {
 
 
 
-
-$(document).ready(function () {
+$(document).ready(function() {
    // Input elemanlarının değerlerini aldığımız değişkenleri tanımlıyoruz.
    var number1 = $('#vip');
    var number2 = $('#firstClass');
@@ -151,40 +146,37 @@ $(document).ready(function () {
    var total = $('#totalsonuc');
 
    // Input elemanlarının değerlerinde değişiklik olduğunda sonucu hesaplayıp yazdıran bir olay işleyicisi ekliyoruz.
-   number1.add(number2).add(number3).on('input', function () {
-      // Input elemanlarının değerlerini alıyoruz.
-      var val1 = number1.val() ? parseFloat(number1.val()) : 0;
-      var val2 = number2.val() ? parseFloat(number2.val()) : 0;
-      var val3 = number3.val() ? parseFloat(number3.val()) : 0;
+   number1.add(number2).add(number3).on('input', function() {
+       // Input elemanlarının değerlerini alıyoruz.
+       var val1 = number1.val() ? parseFloat(number1.val()) : 0;
+       var val2 = number2.val() ? parseFloat(number2.val()) : 0;
+       var val3 = number3.val() ? parseFloat(number3.val()) : 0;
 
-      // Değerleri çarpıp sonucu hesaplıyoruz.
-      var resultValue = (val1 * 7000) + (val2 * 1500) + (val3 * 1000);
-      var indirimlisonuc = ((resultValue * (10) / 100));
-      var totalsonuc = parseFloat((resultValue) - (indirimlisonuc));
+       // Değerleri çarpıp sonucu hesaplıyoruz.
+       var resultValue = (val1 * 7000) + (val2 * 1500) + (val3 * 1000);
+       var indirimlisonuc = ((resultValue * (10) / 100));
+       var totalsonuc = parseFloat((resultValue) - (indirimlisonuc));
 
-      // Sonucu hedef elementine yazdırıyoruz.
-      result.text('$ ' + resultValue);
-      indirim.text('$ ' + indirimlisonuc);
-      total.text('$ ' + totalsonuc);
+       // Sonucu hedef elementine yazdırıyoruz.
+       result.text('$ ' + resultValue);
+       indirim.text('$ ' + indirimlisonuc);
+       total.text('$ ' + totalsonuc);
 
-      // Toplam bilet tutarını localStorage'a kaydediyoruz.
-      localStorage.setItem("totalsonuc", totalsonuc);
+       // Toplam bilet tutarını localStorage'a kaydediyoruz.
+       localStorage.setItem("totalsonuc", totalsonuc);
    });
 });
 
 
 
 
-
- $(document).ready(function() {
+$(document).ready(function() {
    // localStorage'dan hesaplanan bilet tutarını alın
    var totalBiletTutari = localStorage.getItem('totalsonuc');
-   
+
    // "total" id'li p etiketine bilet tutarını yazdırın
    $('#totalBiletYazdirma').text('$: ' + totalBiletTutari);
-   
+
    // localStorage'dan hesaplanan bilet tutarını silin
    localStorage.removeItem('totalsonuc');
-   });
- 
-
+});
